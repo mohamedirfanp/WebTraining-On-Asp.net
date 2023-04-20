0093ffeeca46b7893a4c223512d7fa8a133d6cfb
Mohamed Irfan P <pabi.adn1@gmail.com>
2023-04-20 11:43:20 +0530

    initial commit

---
+++ MVC_App/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using MVC_App.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace MVC_App.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private SqlConnection _connection;
+        private List<StudentModel> _studentList  = new List<StudentModel>();
+        public void Connection()
+        {
+            string conn = _configuration.GetConnectionString("studentDB");
+            _connection = new SqlConnection(conn);
+            _connection.Open();
+        }
+
+        public IActionResult Index()
+        {
+            Connection();
+            //Console.WriteLine(_configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT"));  -> To get the environment
+
+            string selectQuery = "SELECT * FROM StudentDetails";
+            using (SqlCommand cmd = new SqlCommand(selectQuery, _connection))
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    StudentModel studentModel = new StudentModel();
+                    studentModel.Id = (int)reader[0];
+                    studentModel.Name = (string)reader[1];
+                    studentModel.Email = (string)reader[2];
+
+                    _studentList.Add(studentModel);
+                }
+            }
+            ViewBag.Students = _studentList;
+
+            return View(ViewBag);
+        }
+
+    }
+}
---
+++ MVC_App/Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_App.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
